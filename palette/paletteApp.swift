b58b32b20ad84812506a594d2718fc4d786adf2e
debtwealthtrip <debtwealthtrip@gmail.com>
2025-07-22 10:35:18 -0400

    comment previous
    let items = Color.palette.enumerated().map { index, color in
            ScrollTargetItem(color: color, index: index)
        }
    extension
    static let palette: [Color] = [
            .red, .orange, .yellow, .green, .mint, .teal, .blue, .indigo, .purpl

---
+++ palette/paletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  paletteApp.swift
+//  palette
+//
+//  Created by sam win on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct paletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
